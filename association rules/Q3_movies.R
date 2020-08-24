c04a3a5fe8393e5d73a310793ef881fa55bc6758
ranna46 <50590755+ranna46@users.noreply.github.com>
2020-08-25 00:14:34 +0530

    Add files via upload

---
+++ association rules/Q3_movies.R
@@ -0,0 +1,99 @@
+library(arules)
+library(arulesViz)
+library(rmarkdown)
+movies <- read.csv(file.choose())
+View(movies)
+summary(movies)
+str(movies)
+barplot(sapply(movies[,6:15],sum),col=1:10)
+
+###############1##############
+# Applying apriori algorithm to get relevant rules
+rules <- apriori(as.matrix(movies[,6:15]),parameter = list(support=0.004,confidence=0.06,minlen=5))
+inspect(rules)
+inspect(head(sort(rules, by='lift')))
+
+# Sorting rules by confidence 
+rules_conf <- sort(rules,by="confidence")
+inspect(rules_conf)
+# Sorint rules by lift ratio
+rules_lift <- sort(rules,by="lift")
+inspect(rules_lift)
+
+plot(rules,method = "scatterplot")
+plot(rules,method = "grouped")
+plot(rules,method = "graph")
+plot(rules,method = "mosaic")
+
+###############2##############
+# Applying apriori algorithm to get relevant rules
+rules1 <- apriori(as.matrix(movies[,6:15]),parameter = list(support=0.002,confidence=0.05,minlen=4))
+inspect(rules1)
+inspect(head(sort(rules1, by='lift')))
+
+# Sorting rules by confidence 
+rules_conf <- sort(rules1,by="confidence")
+inspect(rules_conf)
+# Sorint rules by lift ratio
+rules_lift <- sort(rules1,by="lift")
+inspect(rules_lift)
+
+plot(rules1,method = "scatterplot")
+plot(rules1,method = "grouped")
+plot(rules1,method = "graph")
+plot(rules1,method = "mosaic")
+
+###############3##############
+# Applying apriori algorithm to get relevant rules
+rules2 <- apriori(as.matrix(movies[,6:15]),parameter = list(support=0.003,confidence=0.05,minlen=5))
+inspect(rules2)
+inspect(head(sort(rules2, by='lift')))
+
+# Sorting rules by confidence 
+rules_conf <- sort(rules2,by="confidence")
+inspect(rules_conf)
+# Sorint rules by lift ratio
+rules_lift <- sort(rules2,by="lift")
+inspect(rules_lift)
+
+plot(rules2,method = "scatterplot")
+plot(rules2,method = "grouped")
+plot(rules2,method = "graph")
+plot(rules2,method = "mosaic")
+
+###############4##############
+# Applying apriori algorithm to get relevant rules
+rules3 <- apriori(as.matrix(movies[,6:15]),parameter = list(support=0.005,confidence=0.06,minlen=4))
+inspect(rules3)
+inspect(head(sort(rules3, by='lift')))
+
+# Sorting rules by confidence 
+rules_conf <- sort(rules3,by="confidence")
+inspect(rules_conf)
+# Sorint rules by lift ratio
+rules_lift <- sort(rules3,by="lift")
+inspect(rules_lift)
+
+plot(rules3,method = "scatterplot")
+plot(rules3,method = "grouped")
+plot(rules3,method = "graph")
+plot(rules3,method = "mosaic")
+
+###############5##############
+# Applying apriori algorithm to get relevant rules
+rules4 <- apriori(as.matrix(movies[,6:15]),parameter = list(support=0.006,confidence=0.06,minlen=5))
+inspect(rules4)
+inspect(head(sort(rules4, by='lift')))
+
+# Sorting rules by confidence 
+rules_conf <- sort(rules4,by="confidence")
+inspect(rules_conf)
+# Sorint rules by lift ratio
+rules_lift <- sort(rules4,by="lift")
+inspect(rules_lift)
+
+plot(rules4,method = "scatterplot")
+plot(rules4,method = "grouped")
+plot(rules4,method = "graph")
+plot(rules4,method = "mosaic")
+
